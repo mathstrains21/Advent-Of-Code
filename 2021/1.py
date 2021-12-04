03860db04a305bceab558e4f4ea1bbfc4860d17c
mathstrains21 <89940630+mathstrains21@users.noreply.github.com>
2021-12-04 10:22:26 +0000

    fix

--- 2021/1.py
+++ 2021/1.py
@@ -1,14 +1,14 @@
 def execute1(puzzle_input):
     increased = 0
-    previous = int(input.splitlines()[0])
-    for number in input.splitlines()[1:]:
+    previous = int(puzzle_input.splitlines()[0])
+    for number in puzzle_input.splitlines()[1:]:
         if int(number) > previous:
             increased += 1
         previous = int(number)
     return increased
 
 def execute2(puzzle_input):
-    nums = [int(n) for n in input.splitlines()]
+    nums = [int(n) for n in puzzle_input.splitlines()]
     return sum(
     a + b + c < b + c + d
     for a, b, c, d in zip(nums, nums[1:], nums[2:], nums[3:])
